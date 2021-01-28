e8e0bc64803edafe341ab5e21a9a2a13a633a739
hoaxdream <hoaxdream@gmail.com>
2021-01-29 03:48:54 +0800

    Removed test

--- Modules/tester.lua
+++ Modules/tester.lua
@@ -2,13 +2,3 @@
 
 --MultiBarBottomLeftButton8:ClearAllPoints()
 --MultiBarBottomLeftButton8:SetAllPoints("LEFT", ActionButton12, "RIGHT", 6, 0)
-
-local xbar = CreateFrame("Frame")
-xbar:RegisterEvent("ADDON_LOADED")
-xbar:SetScript("OnEvent", function()
-  if event == "ADDON_LOADED" and arg1 == "Blizzard_BindingUI" then
-    QuickKeybindFrame.phantomExtraActionButton:ClearAllPoints()
-    QuickKeybindFrame.phantomExtraActionButton:SetPoint("TOPLEFT", -400, -440)
-    QuickKeybindFrame.phantomExtraActionButton:SetPoint("BOTTOMRIGHT", -400, -440)
-  end
-end)
