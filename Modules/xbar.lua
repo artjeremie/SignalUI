0f19bc8b6ce5f7bfca05e31483aec1e352f72bb9
hoaxdream <hoaxdream@gmail.com>
2021-01-29 04:20:20 +0800

    xbar

--- Modules/xbar.lua
+++ Modules/xbar.lua
@@ -1,9 +1,9 @@
 local xbar = CreateFrame("Frame")
-xbar:RegisterEvent("ADDON_LOADED")
-xbar:RegisterEvent("PLAYER_LOGIN")
-xbar:SetScript("OnEvent", function()
+xbar:SetScript("OnEvent", function(self, event, ...)
   if event == "ADDON_LOADED" and arg1 == "Blizzard_BindingUI" then
     QuickKeybindFrame.phantomExtraActionButton:ClearAllPoints()
-    QuickKeybindFrame.phantomExtraActionButton:SetPoint("RIGHT", -4, -4)
+    QuickKeybindFrame.phantomExtraActionButton:SetPoint("RIGHT", -400, -4)
   end
 end)
+xbar:RegisterEvent("ADDON_LOADED")
+xbar:RegisterEvent("PLAYER_LOGIN")
