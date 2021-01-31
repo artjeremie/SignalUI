058210f08aa6d085fe983339d4732f8d06a1a1de
hoaxdream <hoaxdream@gmail.com>
2021-01-31 16:08:49 +0800

    Minor edits

--- Modules/placeholder.lua
+++ Modules/placeholder.lua
@@ -1,1 +1,9 @@
 -- This is for testing purposes
+
+--== ACTION_CAMERA ==--
+--SetCVar("actioncam", on)
+--SetCVar("test_cameraDynamicPitch", 1)
+--SetCVar("test_cameraDynamicPitchBaseFovPad", 0.3)
+
+--UIParent:UnregisterEvent("EXPERIMENTAL_CVAR_CONFIRMATION_NEEDED")
+--++ END ++--
