f0144237ad83e5eca418d1ea8e87932428859239
hoaxdream <hoaxdream@gmail.com>
2021-02-08 19:12:24 +0800

    Minor edit

--- Modules/minimap.lua
+++ Modules/minimap.lua
@@ -8,9 +8,11 @@ Minimap:SetScript("OnMouseWheel", function(self, arg1)
   end
 end)
 
+-- Move zone text
 MinimapZoneText:ClearAllPoints()
 MinimapZoneText:SetPoint('TOPRIGHT', UIParent, "TOPRIGHT", -22, -7)
 MinimapZoneText:SetFont("FONTS\\FRIZQT__.TTF", 10, "OUTLINE")
 
+-- Move minimap
 MinimapCluster:ClearAllPoints()
 MinimapCluster:SetPoint('TOPRIGHT', UIParent, "TOPRIGHT", -5, -14)
